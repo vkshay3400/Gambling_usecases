4799e5bfeaf0635d5f9636ceef55cd7a1e025d09
vkshay3400 <akshayp2208@gmail.com>
2020-02-21 12:47:10 +0530

    Added: Gambling Stake

--- gambling.sh
+++ gambling.sh
@@ -2,3 +2,22 @@
 
 echo "*************************Gambler Simulation*************************"
 
+# VARIABLE
+stake=100
+
+# CONSTANT
+MINIMUM=$(( $stake*50/100 ))
+MAXIMUM=$(( $stake*150/100 ))
+
+# TO GET RANDOM NUMBER
+random=$((RANDOM%2))
+
+# BET A GAMBLER TO GET WIN OR LOSS
+if (( $random == 1 ))
+then
+	((stake++))
+else
+	((stake--))
+fi
+
+
