80c134a2588672975ad610df5fd1dbc02158cb44
vkshay3400 <akshayp2208@gmail.com>
2020-02-21 14:06:56 +0530

    Added: Gambler Stop or Continue

--- gambling.sh
+++ gambling.sh
@@ -12,18 +12,20 @@ DAYS=20
 # VARIABLE
 sum=0
 
+# FUNCTION FOR GAMBLER
+function getGambler(){
 # FOR 20 DAYS
 for (( days=1; days<=DAYS; days++ ))
 do
 	stake=$CASH
-	
+
 	# GAMBLER FOR 50% WIN/LOSS
 	while (( $stake>$MINIMUM && $stake<$MAXIMUM ))
 	do
 
 		# TO GET RANDOM NUMBER
 		random=$((RANDOM%2))
-		
+
 		# TO GET WIN/LOSS
 		if [ $random -eq $WIN ]
 		then
@@ -49,14 +51,27 @@ done
 	echo "Daily Stake value: "${store1[@]}
 	echo "Total:             "${store5[@]}
 
-# TO FIND LUCKIEST 
+# TO FIND LUCKIEST
 for k in ${!store5[@]}
-do 
+do
 	echo $k 'Day_is_Luckiest' ${store5[$k]}
 done | sort -rn -k3 | head -1
 
-# TO FIND UNLUCKIEST 
+# TO FIND UNLUCKIEST
 for k in ${!store5[@]}
-do 
+do
 	echo $k 'Day_is_Unluckiest' ${store5[$k]}
 done | sort -rn -k3 | tail -1
+
+# TO CONTINUE IF WON
+if(($sum>$CASH))
+then
+	echo "You Won and continue play"
+	getGambler
+else
+	echo "You Lose"
+fi
+}
+
+# CALLING FUNCTION
+getGambler
